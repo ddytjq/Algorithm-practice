d5d90316be0537c4c7b6506ec6b736a448293e6c
ddytjq <dytjqg@gmail.com>
2018-12-19 20:58:20 +0900

    커밋

--- programmers/Hash_3.java
+++ programmers/Hash_3.java
@@ -19,30 +19,20 @@ public class Hash_3 {
 		}
 		
 		for(String name : map.keySet()) {
-			if(map.size()==1) {
-				result = 0;
-				answer = map.get(name);
-			}
-			else {
-				result *= map.get(name);
-				answer += map.get(name);
-			}
+			result += result*map.get(name);
 		}
-		answer += result;
+		answer = result-1;
 		
 		return answer;
-
 	}
 
 	public static void main(String[] args) {
 		String[][] clothes = { { "yellow_hat", "headgear" }, { "blue_sunglasses", "eyewear" },
 				{ "green_turban", "headgear" } };
 		String[][] clothes2 = {{"crow_mask", "face"}, {"blue_sunglasses", "face"}, {"smoky_makeup", "face"}};
-		String[][] clothes3 = {{"1","a"},{"2","a"},{"3","a"},{"4","b"},{"5","b"},{"6","c"},{"7","c"}};
 
 		System.out.println(solution(clothes));
 		System.out.println(solution(clothes2));
-		System.out.println(solution(clothes3));
 	}
 
 }
