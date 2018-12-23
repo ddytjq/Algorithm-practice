c86c5a88a20284092d71da9c044fae2819741169
ddytjq <dytjqg@gmail.com>
2018-12-23 21:31:07 +0900

    커밋

---
+++ programmers/Stack_1.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Stack_1 {
+	public static int solution(String arrangement) {
+		int answer = 0;
+		Stack st = new Stack();
+
+		for (int i = 0; i < arrangement.length(); i++) {
+			if (arrangement.charAt(i) == '(') {
+				st.push(1);
+			} else {
+				st.pop();
+				answer += st.size();
+			}
+		}
+
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println(solution(sc.nextLine()));
+	}
+}
