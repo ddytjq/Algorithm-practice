53933a806e0ff8c127a9d3ad729a2ac51fa9c259
ddytjq <dytjqg@gmail.com>
2019-02-12 23:44:27 +0900

    주식가격

--- programmers/Stack_Queue_6.java
+++ programmers/Stack_Queue_6.java
@@ -1,24 +1,24 @@
 public class Stack_Queue_6 {
-    public static int[] solution(int[] prices) {
-        int len = prices.length;
-        int[] answer = new int[len];
-         
-        for(int start = 0; start < len - 1; start++){
-            for(int end = start + 1; end < len; end++){
-                if(prices[start] > prices[end] || end == len - 1){
-                    answer[start] = end - start;
-                    break;
-                }
-            }
-        }
-        answer[len - 1] = 0;
-        return answer;
-    }
-    
-    public static void main(String[] args) {
-    	int [] prices = {498, 501, 470, 489};
-    	
-    	for(int i = 0; i < solution(prices).length; i++)
-    		System.out.print(solution(prices)[i]);	
-    }
+	public static int[] solution(int[] prices) {
+		int[] answer = {};
+		answer = new int[prices.length];
+
+		for (int i = 0; i < prices.length; i++) {
+			answer[i] = prices.length - i -1;
+			for (int j = i + 1; j < prices.length; j++) {
+				if(prices[i] > prices[j]) {
+					answer[i] = j-i;
+					break;
+				}
+			}
+		}
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		int[] prices = { 498, 501, 470, 489 };
+
+		for (int i = 0; i < solution(prices).length; i++)
+			System.out.print(solution(prices)[i] + " ");
+	}
 }
