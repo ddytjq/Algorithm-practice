18cd25ab87139911f83b0ae5f8e04e5f0ac33a66
ddytjq <dytjqg@gmail.com>
2019-02-13 12:59:01 +0900

    IDE 수정 / 더 맵게

--- programmers/Heap_1.java
+++ programmers/Heap_1.java
@@ -1,10 +1,37 @@
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Heap_1 {
-	public static int solution(int[] scoville, int K) {
-		int answer = 0;
-		
-		//더 맵게 진행 
-		return answer;
-	}
+    public static int solution(int[] scoville, int K) {
+        int answer = 0;
+        Queue queue = new LinkedList<>();
+
+        for (int sco : scoville) {
+            if (sco < K)
+                queue.add(sco);
+        }
+
+        while(!queue.isEmpty()){
+            if(queue.size() > 1){
+                int value = (int) queue.poll() + ((int) queue.poll() * 2);
+                if (value < K) {
+                    queue.add(value);
+                }
+                answer++;
+            }
+            else {
+                queue.poll();
+                answer = -1;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] scoville = {1, 2, 3, 9, 10, 12};
+        int K = 7;
+
+        System.out.println(solution(scoville, K));
+    }
 
 }
